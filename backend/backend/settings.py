f77e597c2fc3603c304ac8093c71f6456ea96f49
abhishek10600 <abhi10sheksharma@gmail.com>
2024-04-30 17:22:08 +0530

    Added new deployed links

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = 'django-insecure-17^b**@+e(2@)j(s0yl2$=()79pgks$12%9joza$e&bq4*$di%
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
     os.getenv("DEPLOYED_URL")
 ]
 
